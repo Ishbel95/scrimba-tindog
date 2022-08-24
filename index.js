7a6b925cb888b716dbe5e8e4f265040327a62270
Ishbel95 <ishbel95@hotmail.com>
2022-08-24 10:24:47 +0100

    refactored dogArray, built nextDogProfile function, enabled like button to move dogprofile along

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 import dogs from "/data.js";
 import Dog from "/Dog.js";
-const dogArray = ["rex", "bella", "teddy"];
+const dogArray = [0, 1, 2];
 let hasBeenSwiped = false;
 let hasBeenLiked = false;
-// let isWaiting = false;
+let isWaiting = false;
 
-function getNewDog(data) {
-  const nextDogData = data[dogArray.shift()];
+function getNewDog() {
+  const nextDogData = dogs[dogArray.shift()];
   return nextDogData ? new Dog(nextDogData) : {};
+}
+
+function nextDogProfile() {
+  if (dogArray.length > 0) {
+    newDog = getNewDog();
+    render();
+  } else {
+    document.getElementById("dog").innerHTML = `<p>No more dogs</p>`;
+  }
 }
 
 function render() {
   document.getElementById("dog").innerHTML = newDog.getDogHtml();
 }
+
 document.getElementById("like").addEventListener("click", () => {
   console.log("hello");
   hasBeenLiked = true;
   hasBeenSwiped = false;
+  nextDogProfile();
+});
+document.getElementById("cross").addEventListener("click", () => {
+  console.log("hello");
+  hasBeenLiked = false;
+  hasBeenSwiped = true;
 });
 
-let newDog = new Dog(getNewDog(dogs));
+let newDog = getNewDog();
 render();
 
 // newDog.getDogHtml();
